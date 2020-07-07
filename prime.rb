4a28dca0ab6aac3b556b132197ad28b04355e380
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 22:32:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(given_int)
   return false if given_int <= 1
-    (2..given_int).each do |x|
-      if given_int & x == 0
-        return false
-      end
+  (2..given_int).each do |x|
+    if given_int & x == 0
+      return false
+    end
     end
   end
   return true
